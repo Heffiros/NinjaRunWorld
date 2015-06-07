1fa410577608752c956d4854840071248c852669
Alexandre <levy_a@etna-alternance.net>
2015-06-07 11:11:11 +0200

    stage1

---
+++ Pirate/Assets/Script/gestionMouvement.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gestionMouvement : MonoBehaviour {
+
+	// Use this for initialization
+	public float speed = 0.01f;
+	//private Animator anim;
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 move = Vector3.zero;
+
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			//anim.Play ("Walk");
+			move += Vector3.left;
+			//this.gameObject.transform.position += Vector3.forward * speed;
+		} else if (Input.GetKey (KeyCode.DownArrow)) {
+			move += Vector3.right;
+			//this.gameObject.transform.position += Vector3.back * speed;
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			move+= Vector3.back;
+		} else if (Input.GetKey (KeyCode.RightArrow)) {
+			move+= Vector3.forward;
+		}
+		this.gameObject.transform.position += move * speed * Time.fixedDeltaTime;
+	}
+
+	void OnCollisionEnter(Collision c)
+	{
+		if (c.collider.tag == "ZombieTag") {
+			//Instantiate(this.zombie,this.gameObject.transform.position,this.gameObject.transform.rotation);
+		
+			//Destroy(this.gameObject);
+		}
+	}
+	// Update is called once per frame
+}
